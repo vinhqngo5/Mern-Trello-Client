43fb88a6b255d2c2b98a12979c6148123147862e
vinhqngo5 <vinhqngo5@gmail.com>
2021-09-02 21:26:41 +0700

    Implement dropdown button and show modal when remove column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Column.scss";
 import Card from "components/Card/Card";
 import { mapOrder } from "utilities/sort";
 import { Container, Draggable } from "react-smooth-dnd";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import ConfirmModal from "components/Common/ConfirmModal";
+import { MODAL_ACTION_CLOSE, MODAL_ACTION_CONFIRM } from "utilities/constants";
 
 function Column(props) {
+	const [showConfirmModal, setShowConfirmModal] = useState(false);
 	const { column, onCardDrop } = props;
+
 	const cards = mapOrder(column.cards, column.cardOrder, "id");
 
+	const onConfirmModalAction = (type) => {
+		console.log(type);
+		switch (type) {
+			case MODAL_ACTION_CLOSE:
+				// just close then do nothing
+				break;
+			case MODAL_ACTION_CONFIRM:
+				// remove column
+				break;
+			default:
+		}
+		toggleShowConfirmModal();
+	};
+
+	const toggleShowConfirmModal = () => {
+		setShowConfirmModal(!showConfirmModal);
+	};
 	return (
 		<div className="column">
-			<header className="column-drag-handle">{column.title}</header>
+			<header className="column-drag-handle">
+				<div className="column-title">{column.title}</div>
+				<div className="column-dropdown-actions">
+					<Dropdown>
+						<Dropdown.Toggle
+							id="dropdown-basic"
+							size="sm"
+							className="dropdown-btn"
+						/>
+
+						<Dropdown.Menu>
+							<Dropdown.Item>Add card...</Dropdown.Item>
+							<Dropdown.Item onClick={toggleShowConfirmModal}>
+								Remove Column...
+							</Dropdown.Item>
+							<Dropdown.Item>
+								Move all cards in this column (beta)...
+							</Dropdown.Item>
+							<Dropdown.Item>
+								Arrchive all cards in this column (beta)...
+							</Dropdown.Item>
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
+			</header>
 			<div className="card-list">
 				<Container
 					// onDragStart={(e) => console.log("drag started", e)}
@@ -52,6 +98,12 @@ function Column(props) {
 					&nbsp;Add another card
 				</div>
 			</footer>
+			<ConfirmModal
+				show={showConfirmModal}
+				onAction={onConfirmModalAction}
+				title="Remove column"
+				content={`Are you sure to remove <strong>${column.title}</strong>! <br>All related cards will also be removed`}
+			/>
 		</div>
 	);
 }
---
+++ src/utilities/constants.js
@@ -0,0 +1,5 @@
+/**
+ * constants in ConfirmModal
+ */
+export const MODAL_ACTION_CLOSE = "close";
+export const MODAL_ACTION_CONFIRM = "confirm";
